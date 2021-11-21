447691caf96350c512810695cf74efa959bf1d53
apaneser <56094764+apaneser@users.noreply.github.com>
2021-11-21 12:04:17 -0800

    add styling

--- app.py
+++ app.py
@@ -9,28 +9,26 @@ def index():
     return render_template("index.html")
 
 @app.route('/predict',methods=['POST'])
-def predict():
+def prediction():
     # Get the data from the POST request.
     if request.method == "POST":
-        print(request.form["home-type"])
-        hometype = request.form["home-type"]
-        hasgarage = request.form["hasgarage"]
-        yearbuilt = request.form["yearbuilt"]
-        zipcode = request.form["zipcode"]
+        print('valuecheck: ' + request.form["home-type"])
+        hometype = int(request.form["home-type"])
+        hasgarage = int(request.form["hasgarage"])
+        yearbuilt = int(request.form["yearbuilt"])
+        zipcode = int(request.form["zipcode"])
         lotsizesqft = request.form["lotsizesqft"]
-        livingareasqft = request.form["livingareasqft"]
-        numofbathrooms = request.form["numofbathrooms"]
-        numofbedrooms = request.form["numofbedrooms"]
-        numofstories = request.form["numofstories"]
-        avgschooldistance = request.form["avgschooldistance"]
-  
+        livingareasqft = int(request.form["livingareasqft"])
+        numofbathrooms = float(request.form["numofbathrooms"])
+        numofbedrooms = int(request.form["numofbedrooms"])
+        numofstories = int(request.form["numofstories"])
+        avgschooldistance = int(request.form["avgschooldistance"])
+
         prediction = '${0:,.0f}'.format(predict(hometype, hasgarage, yearbuilt, zipcode, lotsizesqft, 
         livingareasqft, numofbathrooms, numofbedrooms, numofstories, avgschooldistance)[0])
 
-        output = prediction[0]
-
-        print(output)
-        return render_template("results.html", results=output)
+        print(prediction)
+        return render_template("results.html", results=prediction)
 
 if __name__ == "__main__":
     app.run()
